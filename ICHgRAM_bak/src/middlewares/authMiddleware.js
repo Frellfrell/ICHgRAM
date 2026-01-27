4d06ad9b925745ec30960a333f12730403018258
Frellfrell <ella.gubareva.24@gmail.com>
2026-01-27 17:33:01 +0100

    Update authMiddleware.js

--- ICHgRAM_bak/src/middlewares/authMiddleware.js
+++ ICHgRAM_bak/src/middlewares/authMiddleware.js
@@ -1,2 +1,10 @@
 import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
+
+const authMiddleware = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ message: "Нет доступа" });
+    }
