0593119dfb3c10d735d5666907dc9670df5479ea
Frellfrell <ella.gubareva.24@gmail.com>
2026-01-28 12:58:11 +0100

    Update userController.js

--- ICHgRAM_bak/src/controllers/userController.js
+++ ICHgRAM_bak/src/controllers/userController.js
@@ -2,3 +2,12 @@ import User from "../models/userModel.js";
 export const getUserProfile = async (req, res) => {
   try {
     const { userId } = req.params;
+    const user = await User.findById(userId).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
