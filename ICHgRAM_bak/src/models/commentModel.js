f17b7dfe76756a6627d8ddcddf72b6f6ea68ebb0
Frellfrell <ella.gubareva.24@gmail.com>
2026-02-09 22:32:01 +0100

    Update commentModel.js

--- ICHgRAM_bak/src/models/commentModel.js
+++ ICHgRAM_bak/src/models/commentModel.js
@@ -1,16 +1,22 @@
 import mongoose from "mongoose";
-const postSchema = new mongoose.Schema({
+const postSchema = new mongoose.Schema(
+  {
     text: {
-        type: String,
-        required: true,
-        maxlength: 2000,
+      type: String,
+      required: true,
+      maxlength: 2000,
     },
     author: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
     post: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Post",
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+export default mongoose.model("Comment", postSchema);
