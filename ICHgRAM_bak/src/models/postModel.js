4ee1628f8dbb1e9d9279ea46d2d405250d3f77b8
Frellfrell <ella.gubareva.24@gmail.com>
2026-02-09 22:24:00 +0100

    Update postModel.js

--- ICHgRAM_bak/src/models/postModel.js
+++ ICHgRAM_bak/src/models/postModel.js
@@ -1,3 +1,23 @@
 import mongoose from "mongoose";
 
-const postSchema = new mongoose.Schema({
+const postSchema = new mongoose.Schema(
+  {
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    caption: {
+      type: String,
+      maxlength: 2000,
+      default: "",
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("Post", postSchema);
