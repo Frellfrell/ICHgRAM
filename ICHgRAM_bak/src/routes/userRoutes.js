16629a0710984e52074af1613de2f162940344fa
Frellfrell <ella.gubareva.24@gmail.com>
2026-02-09 21:57:46 +0100

    Update userRoutes.js
    import middlewere,profile

--- ICHgRAM_bak/src/routes/userRoutes.js
+++ ICHgRAM_bak/src/routes/userRoutes.js
@@ -1,1 +1,7 @@
 import express from "express";
+import authMiddleware from "../middleware/authMiddleware.js";
+import upload from "../middleware/uploadMiddleware.js";
+import {
+  getUserProfile,
+  updateProfile,
+} from "../controllers/userController.js";
