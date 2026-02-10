8e7680ab83f1551e4fd2b2c852ef59535cb7fa26
Frellfrell <ella.gubareva.24@gmail.com>
2026-02-10 12:08:58 +0100

    Update postController.js

--- ICHgRAM_bak/src/controllers/postController.js
+++ ICHgRAM_bak/src/controllers/postController.js
@@ -31,9 +31,13 @@ export const createPost = async (req, res) => {
 };
 
 export const getAllPosts = async (req, res) => {
-    try {
-        const posts = await Post.find()
-            .populate("author", "username")
-            .sort({ createdAt: -1 });
-        res.json(posts);
-    }
+  try {
+    const posts = await Post.find()
+      .populate("author", "username")
+      .sort({ createdAt: -1 });
+
+    res.json(posts);
+  } catch (error) {
+    res.status(500).json({ message: "Ошибка сервера" });
+  }
+};
