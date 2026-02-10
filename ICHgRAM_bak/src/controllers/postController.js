45a103809953417a85c6283d000bf39747ff8a1c
Frellfrell <ella.gubareva.24@gmail.com>
2026-02-10 12:07:01 +0100

    Update postController.js

--- ICHgRAM_bak/src/controllers/postController.js
+++ ICHgRAM_bak/src/controllers/postController.js
@@ -1,16 +1,34 @@
 import Post from "../models/postModel.js";
 export const createPost = async (req, res) => {
   try {
-    const { text } = req.body;
+    const { caption } = req.body;
 
-     if (!req.file) {
-      return res.status(400).json({ message: "Изображение обязательно" });
+    let image = "";
+
+    // если изображение передано
+    if (req.file) {
+      const base64Image = req.file.buffer.toString("base64");
+      image = `data:${req.file.mimetype};base64,${base64Image}`;
     }
 
-    const base64Image = req.file.buffer.toString("base64");
+    // запрет пустого поста
+    if (!caption && !image) {
+      return res.status(400).json({
+        message: "Пост не может быть пустым",
+      });
+    }
 
     const post = await Post.create({
       author: req.user._id,
       caption,
       image: `data:${req.file.mimetype};base64,${base64Image}`,
     });
+
+    res.status(201).json(post);
+  } catch (error) {
+    res.status(500).json({ message: "Ошибка сервера" });
+  }
+};
+
+export const getAllPosts = async (req, res) => {
+    try {
