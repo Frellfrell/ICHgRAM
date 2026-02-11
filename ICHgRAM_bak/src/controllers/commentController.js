ccb67f609ef30224269f000c4703a23f9313b2a8
Frellfrell <ella.gubareva.24@gmail.com>
2026-02-11 20:08:24 +0100

    Update commentController.js

--- ICHgRAM_bak/src/controllers/commentController.js
+++ ICHgRAM_bak/src/controllers/commentController.js
@@ -2,16 +2,21 @@ import Comment from "../models/commentModel.js";
 
 export const addComment = async (req, res) => {
   try {
-     const { text } = req.body;
+    const { text } = req.body;
     const { postId } = req.params;
 
-
-     if (!text) {
+    if (!text) {
       return res.status(400).json({ message: "Комментарий пустой" });
     }
 
-     const comment = await Comment.create({
+    const comment = await Comment.create({
       text,
       post: postId,
       author: req.user._id,
     });
+
+    res.status(201).json(comment);
+  } catch (error) {
+    res.status(500).json({ message: "Ошибка сервера" });
+  }
+};
