722d4deb824bfe3b04c9aaf28fda36e486281ef6
Frellfrell <ella.gubareva.24@gmail.com>
2026-02-11 20:15:43 +0100

    Update postRoutes.js

--- ICHgRAM_bak/src/routes/postRoutes.js
+++ ICHgRAM_bak/src/routes/postRoutes.js
@@ -3,3 +3,4 @@ router.get("/", getAllPosts);
 router.get("/user/:userId", getUserPosts);
 router.get("/:postId", getPostById);
 router.delete("/:postId", authMiddleware, deletePost);
+router.put("/:postId", authMiddleware, upload.single("image"), updatePost);
