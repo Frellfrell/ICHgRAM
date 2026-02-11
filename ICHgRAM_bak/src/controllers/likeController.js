11de45878807eff9b7497aaf5f26b41cc39f2ee8
Frellfrell <ella.gubareva.24@gmail.com>
2026-02-11 20:01:06 +0100

    Update likeController.js

--- ICHgRAM_bak/src/controllers/likeController.js
+++ ICHgRAM_bak/src/controllers/likeController.js
@@ -2,7 +2,7 @@ import Like from "../models/likeModel.js";
 
 export const toggleLike = async (req, res) => {
   try {
-     const { postId } = req.params;
+    const { postId } = req.params;
 
     const existingLike = await Like.findOne({
       post: postId,
@@ -13,7 +13,13 @@ export const toggleLike = async (req, res) => {
       await existingLike.deleteOne();
       return res.json({ liked: false });
     }
-     await Like.create({
+    await Like.create({
       post: postId,
       user: req.user._id,
     });
+
+    res.json({ liked: true });
+  } catch (error) {
+    res.status(500).json({ message: "Ошибка сервера" });
+  }
+};
