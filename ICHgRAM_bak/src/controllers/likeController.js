486c5aba6bfaaac60ef269ffcea85ace503f9dd7
Frellfrell <ella.gubareva.24@gmail.com>
2026-02-11 19:59:35 +0100

    Update likeController.js

---
+++ ICHgRAM_bak/src/controllers/likeController.js
@@ -0,0 +1,10 @@
+import Like from "../models/likeModel.js";
+
+export const toggleLike = async (req, res) => {
+  try {
+     const { postId } = req.params;
+
+    const existingLike = await Like.findOne({
+      post: postId,
+      user: req.user._id,
+    });
