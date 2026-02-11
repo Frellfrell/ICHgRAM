aaff6047514ba4d8f7720d982a4c2669b9aa6250
Frellfrell <ella.gubareva.24@gmail.com>
2026-02-11 20:05:51 +0100

    Update commentController.js

--- ICHgRAM_bak/src/controllers/commentController.js
+++ ICHgRAM_bak/src/controllers/commentController.js
@@ -4,3 +4,8 @@ export const addComment = async (req, res) => {
   try {
      const { text } = req.body;
     const { postId } = req.params;
+
+
+     if (!text) {
+      return res.status(400).json({ message: "Комментарий пустой" });
+    }
