a4dbc3aa9e06eb2dcb32972da61cf73e8f1b5307
Frellfrell <ella.gubareva.24@gmail.com>
2026-02-12 23:45:49 +0100

    Update server.js

--- ICHgRAM_bak/server.js
+++ ICHgRAM_bak/server.js
@@ -2,9 +2,19 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./src/config/db.js";
 import authRoutes from "./src/routes/authRoutes.js";
+import http from "http";
+import { Server } from "socket.io";
 
 dotenv.config();
 const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
 // Подключаемся к MongoDB
 connectDB();
 // Маршруты аутентификации
