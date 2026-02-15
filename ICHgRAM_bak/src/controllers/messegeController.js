d9880c3607af21b30313014c1aa3bbcc7469d37d
Frellfrell <ella.gubareva.24@gmail.com>
2026-02-15 14:23:24 +0100

    Update messegeController.js

--- ICHgRAM_bak/src/controllers/messegeController.js
+++ ICHgRAM_bak/src/controllers/messegeController.js
@@ -2,3 +2,10 @@ import Message from "../models/messageModel.js";
 export const getConversation = async (req, res) => {
   try {
     const { userId } = req.params;
+
+    const messages = await Message.find({
+      $or: [
+        { sender: req.user._id, receiver: userId },
+        { sender: userId, receiver: req.user._id },
+      ],
+    }).sort({ createdAt: 1 });
