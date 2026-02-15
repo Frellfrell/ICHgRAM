8b904042e95cc3369e48ccd27dc8ac66bcf40ecc
Frellfrell <ella.gubareva.24@gmail.com>
2026-02-15 14:10:28 +0100

    Update socket.js

--- ICHgRAM_bak/src/socket.js
+++ ICHgRAM_bak/src/socket.js
@@ -1,3 +1,8 @@
 import Message from "./models/messageModel.js";
 
+const onlineUsers = new Map();
+
 export const socketHandler = (io) => {
+
+     io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
