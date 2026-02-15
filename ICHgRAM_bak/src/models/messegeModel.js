7b7d89570ba3c2ea90d0fa6981a5a763c52daa45
Frellfrell <ella.gubareva.24@gmail.com>
2026-02-15 13:58:35 +0100

    Update messegeModel.js

--- ICHgRAM_bak/src/models/messegeModel.js
+++ ICHgRAM_bak/src/models/messegeModel.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 
-
 const messageSchema = new mongoose.Schema(
   {
     sender: {
@@ -9,14 +8,18 @@ const messageSchema = new mongoose.Schema(
       required: true,
     },
 
-     receiver: {
+    receiver: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
     },
 
-      text: {
+    text: {
       type: String,
       required: true,
     },
   },
+  { timestamps: true },
+);
+
+export default mongoose.model("Message", messageSchema);
