24c7e0f63052948e17cd8a4dde74ca50cfd4b98a
Frellfrell <ella.gubareva.24@gmail.com>
2026-02-15 13:57:30 +0100

    Update messegeModel.js

--- ICHgRAM_bak/src/models/messegeModel.js
+++ ICHgRAM_bak/src/models/messegeModel.js
@@ -8,3 +8,15 @@ const messageSchema = new mongoose.Schema(
       ref: "User",
       required: true,
     },
+
+     receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+      text: {
+      type: String,
+      required: true,
+    },
+  },
