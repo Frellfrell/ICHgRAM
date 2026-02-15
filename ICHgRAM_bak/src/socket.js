7ee28057342b16c08a7ecd2378e11b3a9bbad26c
Frellfrell <ella.gubareva.24@gmail.com>
2026-02-15 14:32:45 +0100

    Update socket.js

--- ICHgRAM_bak/src/socket.js
+++ ICHgRAM_bak/src/socket.js
@@ -4,7 +4,7 @@ const onlineUsers = new Map();
 
 export const socketHandler = (io) => {
   io.on("connection", (socket) => {
-    console.log("User connected:", socket.id);
+    const userId = socket.user.id;
 
     // пользователь присылает свой userId
     socket.on("join", (userId) => {
