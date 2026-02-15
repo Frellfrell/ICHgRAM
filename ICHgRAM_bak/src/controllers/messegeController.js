2fc3fdfd33799faa6a67853f52a3f30dcc0fe768
Frellfrell <ella.gubareva.24@gmail.com>
2026-02-15 14:23:59 +0100

    Update messegeController.js

--- ICHgRAM_bak/src/controllers/messegeController.js
+++ ICHgRAM_bak/src/controllers/messegeController.js
@@ -9,3 +9,9 @@ export const getConversation = async (req, res) => {
         { sender: userId, receiver: req.user._id },
       ],
     }).sort({ createdAt: 1 });
+
+    res.json(messages);
+  } catch (error) {
+    res.status(500).json({ message: "Ошибка сервера" });
+  }
+};
