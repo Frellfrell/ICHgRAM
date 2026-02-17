53080f9ba164746815710c260c4898b2451d0870
Frellfrell <ella.gubareva.24@gmail.com>
2026-02-17 18:28:48 +0100

    Update followRoutes.js

--- ICHgRAM_bak/src/routes/followRoutes.js
+++ ICHgRAM_bak/src/routes/followRoutes.js
@@ -8,3 +8,10 @@ import {
 import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
+
+router.post("/:userId", authMiddleware, followUser);
+router.delete("/:userId", authMiddleware, unfollowUser);
+router.get("/followers/:userId", getFollowers);
+router.get("/following/:userId", getFollowing);
+
+export default router;
