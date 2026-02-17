62fb22034121b29329978e26a79b2b6851bb0200
Frellfrell <ella.gubareva.24@gmail.com>
2026-02-17 17:53:01 +0100

    Update followModel.js

--- ICHgRAM_bak/src/models/followModel.js
+++ ICHgRAM_bak/src/models/followModel.js
@@ -2,4 +2,8 @@ import mongoose from "mongoose";
 
 
 const followSchema = new mongoose.Schema(
-  {
+  {follower: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
