8b976fec1fc47aa287e2e8fd801db070af9e55d4
Frellfrell <ella.gubareva.24@gmail.com>
2026-02-18 11:32:21 +0100

    Update massageRoutes.js

--- ICHgRAM_bak/src/routes/massageRoutes.js
+++ ICHgRAM_bak/src/routes/massageRoutes.js
@@ -1,3 +1,10 @@
 import express from "express";
 import authMiddleware from "../middleware/authMiddleware.js";
-import { getMessages } from "../controllers/messageController.js";
+import { getConversation } from "../controllers/messageController.js";
+
+const router = express.Router();
+
+// Получить переписку с пользователем
+
+router.get("/:userId", authMiddleware, getConversation);
+export default router;
