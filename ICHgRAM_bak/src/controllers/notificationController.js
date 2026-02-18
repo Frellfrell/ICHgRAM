a521d6bc7f7bae10967385c24633ca405eb9406a
Frellfrell <ella.gubareva.24@gmail.com>
2026-02-18 10:46:25 +0100

    Update notificationController.js

--- ICHgRAM_bak/src/controllers/notificationController.js
+++ ICHgRAM_bak/src/controllers/notificationController.js
@@ -1,1 +1,16 @@
 import Notification from "../models/notificationModel.js";
+export const getNotifications = async (req, res) => {
+  try {
+    const notifications = await Notification.find({
+      recipient: req.user.id,
+    })
+      .populate("sender", "username fullName")
+      .populate("post", "image caption")
+      .sort({ createdAt: -1 });
+
+    res.status(200).json(notifications);
+  } catch (error) {
+    console.error("Ошибка получения уведомлений:", error);
+    res.status(500).json({ message: "Ошибка сервера" });
+  }
+};
