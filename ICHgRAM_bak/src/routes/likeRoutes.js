5cbfafab93d3820ce69dde553753883bf5c8ec71
Frellfrell <ella.gubareva.24@gmail.com>
2026-02-20 16:05:15 +0100

    Update likeRoutes.js

--- ICHgRAM_bak/src/routes/likeRoutes.js
+++ ICHgRAM_bak/src/routes/likeRoutes.js
@@ -1,1 +1,7 @@
+import express from "express";
+import { toggleLike, getPostLikes } from "../controllers/likeController.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
 router.post("/:postId", authMiddleware, toggleLike);
