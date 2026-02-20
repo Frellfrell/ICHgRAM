1af4deac099aba54374443c3b079a66f2bcf639c
Frellfrell <ella.gubareva.24@gmail.com>
2026-02-20 16:03:25 +0100

    Update commentRoutes.js

--- ICHgRAM_bak/src/routes/commentRoutes.js
+++ ICHgRAM_bak/src/routes/commentRoutes.js
@@ -6,6 +6,8 @@ import {
 } from "../controllers/commentController.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
 
+const router = express.Router();
+
 router.post("/:postId", authMiddleware, addComment);
 router.get("/:postId", getPostComments);
 router.delete("/:postId/:commentId", authMiddleware, deleteComment);
