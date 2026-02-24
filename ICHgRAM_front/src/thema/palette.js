0c2963eb2e79345c580d3534e06b77ec6531918c
Frellfrell <ella.gubareva.24@gmail.com>
2026-02-24 17:57:49 +0100

    Update palette.js

---
+++ ICHgRAM_front/src/thema/palette.js
@@ -0,0 +1,31 @@
+export const palette = {
+  background: {
+    default: "#FFFFFF",
+    paper: "#FFFFFF",
+  },
+
+  primary: {
+    main: "rgba(0,149,246,1)",
+    contrastText: "#FFFFFF",
+  },
+
+  secondary: {
+    main: "rgba(77,0,255,1)",
+    contrastText: "#FFFFFF",
+  },
+
+  error: {
+    main: "rgba(255,0,20,1)",
+  },
+  text: {
+    primary: "#000000",
+    secondary: "rgba(115,115,115,1)",
+  },
+
+  divider: "rgba(219,219,219,1)",
+
+  grey: {
+    50: "rgba(250,250,250,1)",
+    100: "rgba(239,239,239,1)",
+  },
+};
