ea5a9e752296dccd6333d9154a76ab5fdf9f75b2
Frellfrell <ella.gubareva.24@gmail.com>
2026-02-24 17:54:45 +0100

    Update theme.js

--- ICHgRAM_front/src/thema/theme.js
+++ ICHgRAM_front/src/thema/theme.js
@@ -2,3 +2,8 @@ import { createTheme } from '@mui/material/styles';
 import { palette } from './palette';
 
 export const theme = createTheme({
+     palette,
+
+  shape: {
+    borderRadius: 8,
+  },
