524f1be6e99c486c019517415e2a00c4c8f95221
Frellfrell <ella.gubareva.24@gmail.com>
2026-02-24 18:00:13 +0100

    Update main.jsx

--- ICHgRAM_front/src/main.jsx
+++ ICHgRAM_front/src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./stales/globals.css";
 import App from "./App.jsx";
 import theme from "./theme/theme.js";
-import { ThemeProvider, CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
