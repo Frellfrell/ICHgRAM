3ff43336bd70b655ae35a757c1de3c029d6bf3f8
Frellfrell <ella.gubareva.24@gmail.com>
2026-02-26 22:33:17 +0100

    Update Sidebar.jsx

---
+++ ICHgRAM_front/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material";
+import SidebarItem from "./SidebarItem";
+import { useTheme } from "@mui/material/styles"
+
+const Sidebar = () => {
+  const theme = useTheme();
+
+
+  
