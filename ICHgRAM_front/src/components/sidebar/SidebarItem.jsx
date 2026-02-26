f273f9a65c806d8b51cf440716ee9bf770c0ed5e
Frellfrell <ella.gubareva.24@gmail.com>
2026-02-26 22:44:49 +0100

    Update SidebarItem.jsx

--- ICHgRAM_front/src/components/sidebar/SidebarItem.jsx
+++ ICHgRAM_front/src/components/sidebar/SidebarItem.jsx
@@ -2,3 +2,6 @@ import { Box } from "@mui/material";
 import { NavLink } from "react-router-dom";
 import AppTypography from "../UI/AppTypography";
 import { useTheme } from "@mui/material/styles";
+
+const SidebarItem = ({ label, to }) => {
+  const theme = useTheme();
