55b026ce62cf638c3ff2a07be35bd21e2e884f33
Frellfrell <ella.gubareva.24@gmail.com>
2026-02-27 21:43:14 +0100

    Update Footer.jsx

--- ICHgRAM_front/src/components/footer/Footer.jsx
+++ ICHgRAM_front/src/components/footer/Footer.jsx
@@ -1,2 +1,13 @@
 import React from "react";
 import { Box, Typography, Link } from "@mui/material";
+
+const Footer = () => {
+
+const links = [
+    { label: "Link1", href: "#" },
+    { label: "Link2", href: "#" },
+    { label: "Link3", href: "#" },
+    { label: "Link4", href: "#" },
+    { label: "Text5", href: null }, // просто текст
+    { label: "Link6", href: "#" },
+  ];
