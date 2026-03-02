d2781236ded5e48b073dad20c914f5b3c35c5a80
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-02 11:25:31 +0100

    Update NotificationDrawer.jsx

--- ICHgRAM_front/src/components/notifications/NotificationDrawer.jsx
+++ ICHgRAM_front/src/components/notifications/NotificationDrawer.jsx
@@ -1,3 +1,11 @@
 import React from "react";
 import { Box, Typography, IconButton } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
+
+
+export const NotificationDrawer = ({ open, onClose, notifications }) => {
+  if (!open) return null;
+
+  return (
+    <>
+    </>
