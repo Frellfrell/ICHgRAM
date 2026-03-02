b785d4d174ac1a5074b96a89e2626ab4f3b7dbbb
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-02 11:46:06 +0100

    Update SearchDrawer.jsx

--- ICHgRAM_front/src/components/search/SearchDrawer.jsx
+++ ICHgRAM_front/src/components/search/SearchDrawer.jsx
@@ -6,4 +6,17 @@ import CloseIcon from "@mui/icons-material/Close";
 export const SearchDrawer = ({ open, onClose, results }) => {
   if (!open) return null;
 
-  
+  return (
+    <>
+    <Box
+        onClick={onClose}
+        sx={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100vw",
+          height: "100vh",
+          backgroundColor: "rgba(0,0,0,0.65)",
+          zIndex: 999,
+        }}
+      />
