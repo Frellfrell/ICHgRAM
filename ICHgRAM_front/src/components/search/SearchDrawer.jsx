9cd73f8250f4cf4c7e8dad93d159e44a48546533
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-02 11:44:32 +0100

    Update SearchDrawer.jsx

--- ICHgRAM_front/src/components/search/SearchDrawer.jsx
+++ ICHgRAM_front/src/components/search/SearchDrawer.jsx
@@ -1,3 +1,9 @@
 import React from "react";
 import { Box, Typography, IconButton, InputBase } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
+
+
+export const SearchDrawer = ({ open, onClose, results }) => {
+  if (!open) return null;
+
+  
