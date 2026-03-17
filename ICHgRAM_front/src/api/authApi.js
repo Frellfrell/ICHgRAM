7be45d2217e0b68da993606efa7daeee7c433652
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-17 15:15:34 +0100

    Update authApi.js

---
+++ ICHgRAM_front/src/api/authApi.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+// 1.  регистрация
+export const registerUser = async (userData) => {
+  try {
+    const response = await axios.post(`${BASE_URL}/auth/register`, userData);
+    return response.data; // Возвращаем данные { message, token }
+  } catch (error) {
+    throw error.response?.data || { message: "Server error" }; // Выбрасываем ошибку дальше, чтобы компонент её поймал
+  }
+};
+
+// 2. логин
+export const loginUser = async (credentials) => {
+  try {
+    const response = await axios.post(`${BASE_URL}/auth/login`, credentials);
+
+    if (response.data.token) {
+      localStorage.setItem("token", response.data.token);
+    }
+
+    return response.data;
+  } catch (error) {
+    throw error.response?.data || { message: "Invalid credentials" };
+  }
+};
