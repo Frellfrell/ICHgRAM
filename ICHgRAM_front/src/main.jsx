bc746e3cc82c076667335e99e329def6e4e3d27f
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-17 22:01:34 +0100

    Update main.jsx

--- ICHgRAM_front/src/main.jsx
+++ ICHgRAM_front/src/main.jsx
@@ -3,14 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./stales/globals.css";
 import App from "./App.jsx";
 import theme from "./theme/theme.js";
-import { ThemeProvider } from "@mui/material/styles";
-import { CssBaseline } from "@mui/material";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App theme={theme} />
-    </ThemeProvider>
+    <App theme={theme} />
   </React.StrictMode>,
 );
