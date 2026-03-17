0ab99d27c1a792d9425b7bb2877a70c020302ca5
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-17 22:33:05 +0100

    Update main.jsx

--- ICHgRAM_front/src/main.jsx
+++ ICHgRAM_front/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./stales/globals.css";
 import App from "./App.jsx";
-import theme from "./theme/theme.js";
+import { theme } from "./theme/theme.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
