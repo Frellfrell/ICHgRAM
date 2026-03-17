aac4041da1210961bc6b3714a39b3e622adb7c0d
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-17 17:51:22 +0100

    Update ResetPassword.jsx

--- ICHgRAM_front/src/pages/auth/ResetPassword.jsx
+++ ICHgRAM_front/src/pages/auth/ResetPassword.jsx
@@ -3,3 +3,7 @@ import AppInput from "../../components/ui/AppInput.jsx";
 import AppButton from "../../components/ui/AppButton.jsx";
 import AppTypography from "../../components/ui/AppTypography.jsx";
 import DividerLine from "../../components/ui/DividerLine.jsx";
+
+const ResetPassword = () => {
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column", gap: "10px" }}>
