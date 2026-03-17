5c962b5a24089bae6ca490a6354737ac08a7b009
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-17 14:15:23 +0100

    Update Register.jsx

---
+++ ICHgRAM_front/src/pages/auth/Register.jsx
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+import { Box, Link } from "@mui/material";
+import AppInput from "../../components/UI/AppInput";
+import AppButton from "../../components/UI/AppButton";
+import AppTypography from "../../components/UI/AppTypography";
