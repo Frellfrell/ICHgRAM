85bc2ab43aff75557c6f7a05adc0819daa26dd11
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-17 22:05:16 +0100

    Update Login.jsx

--- ICHgRAM_front/src/pages/auth/Login.jsx
+++ ICHgRAM_front/src/pages/auth/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Box, Link } from "@mui/material";
 import AppInput from "../components/ui/AppInput.jsx";
 import AppButton from "../components/ui/AppButton.jsx";
