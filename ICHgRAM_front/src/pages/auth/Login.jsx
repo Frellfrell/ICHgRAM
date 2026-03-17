2db0960c452551246dc40617f95d0c6673b1f807
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-17 13:58:08 +0100

    Update Login.jsx

--- ICHgRAM_front/src/pages/auth/Login.jsx
+++ ICHgRAM_front/src/pages/auth/Login.jsx
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import { Box, Typography, Container, Paper, Link } from "@mui/material";
+import React, { useState } from "react";
+import { Box, Typography, Link } from "@mui/material";
 import AppInput from "../components/ui/AppInput.jsx";
 import AppButton from "../components/ui/AppButton.jsx";
-import AppTypography from '../../components/UI/AppTypography'; 
-import DividerLine from '../../components/UI/DividerLine';
+import AppTypography from "../../components/UI/AppTypography";
+import DividerLine from "../../components/UI/DividerLine";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLogin = (e) => {
     e.preventDefault();
@@ -26,67 +26,75 @@ const Login = () => {
       }}
     >
       {/* Основной блок формы */}
-      <Box sx={{ 
-        width: 350, 
-        border: '1px solid #dbdbdb', 
-        bgcolor: 'white', 
-        p: '40px 20px', 
-        textAlign: 'center' 
-      }}>
-         <Box 
-          component="img" 
-          src="/assets/logo/ICHGRA 5.svg" 
-          sx={{ width: 190, height: 106, mb: 3, objectFit: 'contain' }} 
+      <Box
+        sx={{
+          width: 350,
+          border: "1px solid #dbdbdb",
+          bgcolor: "white",
+          p: "40px 20px",
+          textAlign: "center",
+        }}
+      >
+        <Box
+          component="img"
+          src="/assets/logo/ICHGRA 5.svg"
+          sx={{
+            width: 190,
+            height: 106,
+            mt: "30px",
+            mb: "24px",
+            objectFit: "contain",
+          }}
         />
 
-          <form onSubmit={handleLogin}>
-            <AppInput placeholder="Username, or email"
+        <form onSubmit={handleLogin}>
+          <AppInput
+            placeholder="Username, or email"
             value={email}
-            onChange={(e) => setEmail(e.target.value)} />
-            <AppInput placeholder="Password" type="password"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <AppInput
+            placeholder="Password"
+            type="password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)} />
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-            <AppButton type="submit" sx={{ mt: 1 }}>
-              Log in
-              </AppButton>
-          </form>
+          <AppButton type="submit" sx={{ mt: 1 }}>
+            Log in
+          </AppButton>
+        </form>
 
-          <DividerLine />
+        <DividerLine />
 
-          
-
-          <Link
-            href="#"
-            underline="none"
-            sx={{ fontSize: 12, color: "#00376b" }}
-          >
-            Forgot password?
-          </Link>
+        <Link href="#" underline="none" sx={{ fontSize: 12, color: "#00376b" }}>
+          Forgot password?
+        </Link>
       </Box>
 
-        {/* Нижний блок с переходом на регистрацию*/}
-       <Box sx={{ 
-        width: 350, 
-        border: '1px solid #dbdbdb', 
-        bgcolor: 'white', 
-        py: 2, 
-        textAlign: 'center' 
-      }}>
+      {/* Нижний блок с переходом на регистрацию*/}
+      <Box
+        sx={{
+          width: 350,
+          border: "1px solid #dbdbdb",
+          bgcolor: "white",
+          py: 2,
+          textAlign: "center",
+        }}
+      >
         <AppTypography variant="body2">
-            Don't have an account?{" "}
-            <Link
-              href="/signup"
-              underline="none"
-              sx={{ fontWeight: 600, color: "primary.main" }}
-            >
-              Sign up
-            </Link>
-          </Typography>
-        </Paper>
-      </Container>
+          Don't have an account?{" "}
+          <Link
+            href="/register"
+            underline="none"
+            sx={{ fontWeight: 600, color: "primary.main" }}
+          >
+            Sign up
+          </Link>
+        </AppTypography>
+      </Box>
     </Box>
   );
 };
 
-export default LoginPage;
+export default Login;
