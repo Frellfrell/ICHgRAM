12088449ece084ebfca7d08796b3643ab44b24af
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-22 23:59:49 +0100

    Update App.jsx

--- ICHgRAM_front/src/App.jsx
+++ ICHgRAM_front/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
           <Route
             path="/login"
             element={
-              <AuthLayout isLogin>
+              <AuthLayout isLogin={true} isReset={false}>
                 <Login />
               </AuthLayout>
             }
