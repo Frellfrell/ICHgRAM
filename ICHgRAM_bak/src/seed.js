4f6922ee7451f786a55f3b56ca13c9fd7264f200
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-23 14:12:05 +0100

    rename svg

--- ICHgRAM_bak/src/seed.js
+++ ICHgRAM_bak/src/seed.js
@@ -17,3 +17,49 @@ const seedData = async () => {
 
     const salt = await bcrypt.genSalt(10);
     const hashedEmailPassword = await bcrypt.hash("password123", salt);
+
+    // 1. Создаем "ботов" из Figma
+    const users = await User.insertMany([
+      {
+        username: "sashaa_designer",
+        fullName: "sashaa",
+        email: "sashaa@example.com",
+        password: hashedEmailPassword,
+        avatar: "/avatars/avatar1.jpg", // Ссылка на твою папку public/avatar
+        bio: "UI/UX Designer from Figma world"
+      },
+      {
+        username: "nikiita_artist",
+        fullName: "nikiita",
+        email: "nikita@example.com",
+        password: hashedEmailPassword,
+        avatar: "/avatars/avatar2.jpg",
+        bio: "Exploring the world one pixel at a time"
+      }
+      {
+        username: "ICHgRAM_official",
+        fullName: "itcareerhub",
+        email: "ICHgRAM@example.com",
+        password: hashedEmailPassword,
+        avatar: "/avatars/avatar2.jpg",
+        bio: "Exploring the world one pixel at a time"
+      }
+      {
+        username: "coach.tonia",
+        fullName: "tonia",
+        email: "tonia@example.com",
+        password: hashedEmailPassword,
+        avatar: "/avatars/avatar2.jpg",
+        bio: "Exploring the world one pixel at a time"
+      }
+      {
+        username: "fsssociety",
+        fullName: "fsssociety",
+        email: "fsssociety@example.com",
+        password: hashedEmailPassword,
+        avatar: "/avatars/avatar2.jpg",
+        bio: "Exploring the world one pixel at a time"
+      }
+    ]);
+
+    console.log("✅ Users created");
