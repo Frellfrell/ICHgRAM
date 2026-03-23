93fe6e3c70bda4d66ec817d8293aeff544bbf60b
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-23 16:21:12 +0100

    Update App.jsx

--- ICHgRAM_front/src/App.jsx
+++ ICHgRAM_front/src/App.jsx
@@ -14,29 +14,43 @@ import Register from "./pages/auth/Register.jsx";
 //import NotFound from "./pages/NotFound.jsx";
 import AuthLayout from "./layout/AuthLayout.jsx";
 import ResetPassword from "./pages/auth/ResetPassword.jsx";
+import Home from "./pages/home/Home.jsx";
+import MainLayout from "./layout/MainLayout.jsx";
 
 import "./App.css";
 
 function App() {
+  // Проверка наличия токена для  аутентификации
+  const isAuthenticated = !!localStorage.getItem("token");
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Router>
         <Routes>
+          {/* Публичные роуты (Auth) */}
           <Route
             path="/login"
             element={
-              <AuthLayout>
-                <Login />
-              </AuthLayout>
+              !isAuthenticated ? (
+                <AuthLayout>
+                  <Login />
+                </AuthLayout>
+              ) : (
+                <Navigate to="/home" />
+              )
             }
           />
           <Route
             path="/register"
             element={
-              <AuthLayout>
-                <Register />
-              </AuthLayout>
+              !isAuthenticated ? (
+                <AuthLayout>
+                  <Register />
+                </AuthLayout>
+              ) : (
+                <Navigate to="/home" />
+              )
             }
           />
 
@@ -49,6 +63,25 @@ function App() {
                 <ResetPassword />
               </AuthLayout>
             }
+          />
+          {/* Приватные роуты (Контент) */}
+          <Route
+            path="/home"
+            element={
+              isAuthenticated ? (
+                <MainLayout>
+                  <Home />
+                </MainLayout>
+              ) : (
+                <Navigate to="/login" />
+              )
+            }
+          />
+
+          {/* Редирект с главной */}
+          <Route
+            path="/"
+            element={<Navigate to={isAuthenticated ? "/home" : "/login"} />}
           />
 
           {/* Страница 404 */}
