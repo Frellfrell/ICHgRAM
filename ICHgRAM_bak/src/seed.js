44381f39ac6ea8946e9cd33ca69f52de39b184fb
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-23 17:42:00 +0100

    Update seed.js

--- ICHgRAM_bak/src/seed.js
+++ ICHgRAM_bak/src/seed.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import bcrypt from "bcryptjs";
-import User from "./src/models/userModel.js";
-import Post from "./src/models/postModel.js";
+import User from "./models/userModel.js";
+import Post from "./models/postModel.js";
 import connectDB from "./src/config/db.js";
 
 dotenv.config();
