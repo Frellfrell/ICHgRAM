f8c879039371df92409ea1b3da232e046f294315
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-23 11:12:58 +0100

    Updad AithLayout and ResetPassword

--- ICHgRAM_front/src/pages/auth/ResetPassword.jsx
+++ ICHgRAM_front/src/pages/auth/ResetPassword.jsx
@@ -7,89 +7,84 @@ import AuthLayout from "../../layout/AuthLayout.jsx";
 
 const ResetPassword = () => {
   return (
-    <AuthLayout isReset={true} isLogin={false}>
-      {/* Контейнер 935x733 */}
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "10px",
+      }}
+    >
       <Box
         sx={{
+          width: "100%",
+          minHeight: "537px",
+          border: "1px solid #dbdbdb",
+          bgcolor: "white",
+          pt: "24px",
+          textAlign: "center",
           display: "flex",
           flexDirection: "column",
-          margin: 0,
-          padding: 0,
-          gap: "10px",
+          mx: "auto",
         }}
       >
+        {/* Иконка замка  */}
+        <Box
+          component="img"
+          src="/src/assets/icons/Img - Trouble logging in_.svg"
+          sx={{
+            width: "96px",
+            height: "auto",
+            border: "2px solid black",
+            borderRadius: "50%",
+            mx: "auto",
+            mb: 2,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        ></Box>
+
+        <AppTypography variant="h6" sx={{ fontWeight: 600, mb: 1 }}>
+          Trouble logging in?
+        </AppTypography>
+
+        <Box sx={{ px: "40px", mb: 2 }}>
+          <AppTypography variant="body2" color="text.secondary">
+            Enter your email, phone, or username and we'll send you a link to
+            get back into your account.
+          </AppTypography>
+        </Box>
+
+        <Box sx={{ px: "40px" }}>
+          <AppInput placeholder="Email or Username" />
+          <AppButton sx={{ mt: 1 }}>Send login link</AppButton>
+
+          <DividerLine />
+        </Box>
+
+        <Link href="/register" underline="none">
+          <AppTypography variant="body2" sx={{ fontWeight: 600, mb: 2 }}>
+            Create new account
+          </AppTypography>
+        </Link>
+
+        {/* Футер формы */}
         <Box
           sx={{
-            width: "100%",
-            minHeight: "537px",
-            border: "1px solid #dbdbdb",
-            bgcolor: "white",
-            pt: "24px",
-            textAlign: "center",
-            display: "flex",
-            flexDirection: "column",
-            mx: "auto",
+            bgcolor: "#fafafa",
+            borderTop: "1px solid #dbdbdb",
+            py: "12px",
+            mt: "auto",
           }}
         >
-          {/* Иконка замка  */}
-          <Box
-            component="img"
-            src="/src/assets/icons/Img - Trouble logging in_.svg"
-            sx={{
-              width: "96px",
-              height: "auto",
-              border: "2px solid black",
-              borderRadius: "50%",
-              mx: "auto",
-              mb: 2,
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          ></Box>
-
-          <AppTypography variant="h6" sx={{ fontWeight: 600, mb: 1 }}>
-            Trouble logging in?
-          </AppTypography>
-
-          <Box sx={{ px: "40px", mb: 2 }}>
-            <AppTypography variant="body2" color="text.secondary">
-              Enter your email, phone, or username and we'll send you a link to
-              get back into your account.
-            </AppTypography>
-          </Box>
-
-          <Box sx={{ px: "40px" }}>
-            <AppInput placeholder="Email or Username" />
-            <AppButton sx={{ mt: 1 }}>Send login link</AppButton>
-
-            <DividerLine />
-          </Box>
-
-          <Link href="/register" underline="none">
-            <AppTypography variant="body2" sx={{ fontWeight: 600, mb: 2 }}>
-              Create new account
+          <Link href="/login" underline="none">
+            <AppTypography variant="body2" sx={{ fontWeight: 600 }}>
+              Back to login
             </AppTypography>
           </Link>
-
-          {/* Футер формы */}
-          <Box
-            sx={{
-              bgcolor: "#fafafa",
-              borderTop: "1px solid #dbdbdb",
-              py: "12px",
-              mt: "auto",
-            }}
-          >
-            <Link href="/login" underline="none">
-              <AppTypography variant="body2" sx={{ fontWeight: 600 }}>
-                Back to login
-              </AppTypography>
-            </Link>
-          </Box>
         </Box>
       </Box>
-    </AuthLayout>
+    </Box>
   );
 };
 
