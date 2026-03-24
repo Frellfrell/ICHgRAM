12ebf9012c4babc664cbff0410d387c6fdc2c76a
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-24 16:00:23 +0100

    Update db.js

--- ICHgRAM_bak/src/config/db.js
+++ ICHgRAM_bak/src/config/db.js
@@ -2,14 +2,5 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB connected");
-  } catch (error) {
-    console.error("MongoDB connection error:", error);
-    process.exit(1); // Остановка приложения при ошибке подключения
-  }
-};
-module.exports = connectDB;
+   const conn = await mongoose.connect(process.env.MONGODB_URI);
+   console.log(`
