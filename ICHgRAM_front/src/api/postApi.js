ac4f60e19b24d8f9e9f0b1a1d8a10c9d6983d431
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-24 22:37:22 +0100

    Update postApi.js

--- ICHgRAM_front/src/api/postApi.js
+++ ICHgRAM_front/src/api/postApi.js
@@ -1,15 +1,14 @@
-import axios from "axios";
 import axiosInstance from "./axiosInstance";
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-export const getAllPosts = async () => {
-  const token = localStorage.getItem("token");
-  const response = await axios.get(`${BASE_URL}/api/posts`, {
-    headers: { Authorization: `Bearer ${token}` },
+/*export const getAllPosts = async () => {
+  //const token = localStorage.getItem("token");
+  //const response = await axios.get(`${BASE_URL}/api/posts`, {
+    //headers: { Authorization: `Bearer ${token}` },
   });
   return response.data; // Получаем массив постов из seed.js
-};
+};*/
 
 export const fetchAllPosts = async () => {
   try {
