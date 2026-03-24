5f2bdbb0f865acf2203268442ad5d5bbf88a48d6
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-24 17:13:42 +0100

    Update userModel.js

--- ICHgRAM_bak/src/models/userModel.js
+++ ICHgRAM_bak/src/models/userModel.js
@@ -7,15 +7,12 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
       unique: true,
-      trim: true,
       minlength: 3,
     },
     fullName: {
       type: String,
       required: true,
-      trim: true,
-      lowercase: true,
-      trim: true,
+      unique: true,
     },
     bio: {
       type: String,
@@ -39,4 +36,11 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true },
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 export default mongoose.model("User", userSchema);
