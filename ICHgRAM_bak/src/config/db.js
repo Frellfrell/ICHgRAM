f184dac7a7dc30b95ee78df620383876f68af588
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-24 15:58:58 +0100

    Update db.js

--- ICHgRAM_bak/src/config/db.js
+++ ICHgRAM_bak/src/config/db.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
+import mongoose from "mongoose";
+
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
