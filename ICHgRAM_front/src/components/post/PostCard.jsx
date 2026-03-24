190f1d138bdeab70581b9601227332eb2fc795a2
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-24 15:43:20 +0100

    Update PostCard.jsx

--- ICHgRAM_front/src/components/post/PostCard.jsx
+++ ICHgRAM_front/src/components/post/PostCard.jsx
@@ -5,3 +5,8 @@ import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 import AppTypography from "../ui/AppTypography";
 import AppAvatar from "../ui/AppAvatar";
 import AppButton from "../ui/AppButton";
+
+const PostCard = ({ post }) => {
+
+    const BE_URL = process.env.VITE_APP_URL || "http://localhost:5000";
+
