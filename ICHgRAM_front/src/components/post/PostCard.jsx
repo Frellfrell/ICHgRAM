3babeb4602d8281702a588064afb59984415a593
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-24 15:36:42 +0100

    Update PostCard.jsx

---
+++ ICHgRAM_front/src/components/post/PostCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Box, IconButton } from "@mui/material";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import AppTypography from "../ui/AppTypography";
+import AppAvatar from "../ui/AppAvatar";
+import AppButton from "../ui/AppButton";
