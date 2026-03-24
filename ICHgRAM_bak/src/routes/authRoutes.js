eea76e453e74acb80ad0e3fa152437aead27b5e0
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-24 22:00:34 +0100

    Update authRoutes.js

--- ICHgRAM_bak/src/routes/authRoutes.js
+++ ICHgRAM_bak/src/routes/authRoutes.js
@@ -1,5 +1,9 @@
 import express from "express";
-import { register, login } from "../controllers/authController.js";
+import {
+  register,
+  login,
+  resetPassword,
+} from "../controllers/authController.js";
 
 const router = express.Router();
 
