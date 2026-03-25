b8d8d1ac3152e0742647e706fe82dc394e661bbe
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-25 13:52:04 +0100

    Update Home.jsx

--- ICHgRAM_front/src/pages/home/Home.jsx
+++ ICHgRAM_front/src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import PostCard from "../../components/post/PostCard.jsx";
 import { fetchAllPosts } from "../../api/postApi";
 import { Box, CircularProgress, Grid } from "@mui/material";
+import HomeEndBlock from "../../components/ui/HomeEndBlock.jsx";
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
@@ -11,7 +12,11 @@ const Home = () => {
     const getPosts = async () => {
       try {
         const data = await fetchAllPosts();
-        setPosts(data);
+        const sashaOnly = data.filter(
+          (post) => post.author?.username === "sasha",
+        );
+        setPosts(sashaOnly);
+        //setPosts(data);
       } catch (err) {
         console.error("Failed to load posts", err);
       } finally {
@@ -33,13 +38,14 @@ const Home = () => {
   return (
     <Box sx={{ width: "100%", maxWidth: "935px", mx: "auto", pt: 5 }}>
       {/* Сетка по 2 поста в ряд (xs=12 для мобилки, sm=6 для десктопа) */}
-      <Grid container spacing={3}>
+      <Grid container spacing={4}>
         {posts.map((post) => (
-          <Grid item xs={12} sm={6} key={post._id}>
+          <Grid item xs={6} sm={4} key={post._id}>
             <PostCard post={post} />
           </Grid>
         ))}
       </Grid>
+      <HomeEndBlock />
     </Box>
   );
 };
