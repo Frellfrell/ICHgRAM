bd449f15dd84f48c62d1703fa3b0fe0006aff03e
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-25 13:43:04 +0100

    Update HomeEndBlock.jsx

--- ICHgRAM_front/src/components/ui/HomeEndBlock.jsx
+++ ICHgRAM_front/src/components/ui/HomeEndBlock.jsx
@@ -1,1 +1,32 @@
 import { Box, Typography } from "@mui/material";
+import EndIcon from "../../assets/logo/endIconHome.svg";
+
+const HomeEndBlock = () => {
+  return (
+    <Box
+      sx={{
+        width: "404px",
+        height: "216px",
+        mx: "auto",
+        mt: 8,
+        mb: 4,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        //borderTop: "1px solid #DBDBDB",
+        py: 4,
+      }}
+    >
+      <img src={EndIcon} alt="End" style={{ width: "83px" }} />
+      <Typography variant="h4" sx={{ textAlign: "center", mt: "11px" }}>
+        You've seen all the updates
+      </Typography>
+      <Typography variant="h6" sx={{ textAlign: "center", mt: "2px" }}>
+        You have viewed all new publications
+      </Typography>
+    </Box>
+  );
+};
+
+export default HomeEndBlock;
