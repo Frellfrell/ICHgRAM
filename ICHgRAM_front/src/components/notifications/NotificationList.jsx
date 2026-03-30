bb940e992b9b94acfc81dd79b3e498a72b315f17
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-30 14:54:05 +0200

    Update NotificationList.jsx

--- ICHgRAM_front/src/components/notifications/NotificationList.jsx
+++ ICHgRAM_front/src/components/notifications/NotificationList.jsx
@@ -5,3 +5,15 @@ import AppAvatar from "../ui/AppAvatar";
 
 const NotificationList = ({ notifications }) => {
   const BE_URL = "http://localhost:5000";
+
+
+  const getActionText = (type) => {
+    switch (type) {
+      case "like": return "liked your photo.";
+      case "follow": return "started following .";
+      case "comment": return "commented on your photo.";
+      default: return "interacted with you.";
+    }
+  };
+
+    return (
