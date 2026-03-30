5b35642a750aeb66790aeb2d22a21a7d038eda21
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-30 21:39:53 +0200

    Update NotFound.jsx

--- ICHgRAM_front/src/pages/NotFound.jsx
+++ ICHgRAM_front/src/pages/NotFound.jsx
@@ -2,3 +2,5 @@ import React from "react";
 import { Box, Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import MainLayout from "../layouts/MainLayout";
+import PhoneFrame from "../assets/foto/phones-frame.png";
+import ScreenShot from "../assets/foto/screenshot1.png";
