9ab43ba792c45186b0c13c7ff35b608c128dc38a
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-30 14:17:33 +0200

    Update SearchResults.jsx

--- ICHgRAM_front/src/components/search/SearchResults.jsx
+++ ICHgRAM_front/src/components/search/SearchResults.jsx
@@ -7,3 +7,7 @@ const SearchResults = ({ results, onUserClick }) => {
   const BE_URL = "http://localhost:5000";
 
   return (
+    <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+              {results?.map((user) => (
+                <Box
+                  key={user._id}
