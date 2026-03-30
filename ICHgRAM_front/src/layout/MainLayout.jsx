f386b5cb07dc529abf439ddf1dc7289ec09e4d04
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-30 18:22:33 +0200

    Update MainLayout.jsx

--- ICHgRAM_front/src/layout/MainLayout.jsx
+++ ICHgRAM_front/src/layout/MainLayout.jsx
@@ -20,12 +20,19 @@ const MainLayout = ({ children }) => {
     if (!query) return setSearchResults([]);
     try {
       const token = localStorage.getItem("token"); // если есть авторизация
-      const res = await axios.get(`http://localhost:5000/api/search?query=${query}`, {
-        headers: { Authorization: `Bearer ${token}` }
-      });
+      const res = await axios.get(
+        `http://localhost:5000/api/search?query=${query}`,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        },
+      );
+      setSearchResults(res.data);
+    } catch (err) {
+      console.error("Search error:", err);
+      setSearchResults([]);
+    }
+  };
 
-
-      
   return (
     <Box
       sx={{
