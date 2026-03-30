7988d3dfe54c2c2cc98e8bbdf97838b4f10fcafd
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-30 14:42:16 +0200

    Update searchController.js

--- ICHgRAM_bak/src/controllers/searchController.js
+++ ICHgRAM_bak/src/controllers/searchController.js
@@ -1,7 +1,7 @@
 import User from "../models/userModel.js";
 
 // Контроллер для поиска пользователей
-export const searchUsers = async (req, res) => {
+export const searchController = async (req, res) => {
   try {
     const { query } = req.query;
 
