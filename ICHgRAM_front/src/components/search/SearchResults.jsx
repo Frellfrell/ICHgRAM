1ca5683c2e0b710add430c698436959db35114af
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-30 14:14:55 +0200

    Update SearchResults.jsx

--- ICHgRAM_front/src/components/search/SearchResults.jsx
+++ ICHgRAM_front/src/components/search/SearchResults.jsx
@@ -1,3 +1,9 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
 import AppAvatar from "../ui/AppAvatar";
+
+
+const SearchResults = ({ results, onUserClick }) => {
+  const BE_URL = "http://localhost:5000";
+
+  
