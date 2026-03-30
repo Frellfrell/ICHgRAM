9c787f3e5f4274b08531e645d98142e57aac0ed0
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-30 14:46:59 +0200

    Update NotificationList.jsx

--- ICHgRAM_front/src/components/notifications/NotificationList.jsx
+++ ICHgRAM_front/src/components/notifications/NotificationList.jsx
@@ -1,3 +1,7 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
 import AppAvatar from "../ui/AppAvatar";
+
+
+const NotificationList = ({ notifications }) => {
+  const BE_URL = "http://localhost:5000";
