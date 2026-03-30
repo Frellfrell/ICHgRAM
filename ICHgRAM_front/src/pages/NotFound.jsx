54a8838971355d59cb3095969f5d7356d58c9474
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-30 22:16:07 +0200

    Updad files

--- ICHgRAM_front/src/pages/NotFound.jsx
+++ ICHgRAM_front/src/pages/NotFound.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
-import MainLayout from "../layouts/MainLayout";
+import MainLayout from "../layout/MainLayout";
 import PhoneFrame from "../assets/foto/phones-frame.png";
 import ScreenShot from "../assets/foto/screenshot1.png";
 
