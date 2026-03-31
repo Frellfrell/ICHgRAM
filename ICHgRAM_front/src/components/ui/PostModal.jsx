fc9bf245dfeb41929b247e36980671361e2dc9fa
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-31 15:15:56 +0200

    Update PostModal.jsx

--- ICHgRAM_front/src/components/ui/PostModal.jsx
+++ ICHgRAM_front/src/components/ui/PostModal.jsx
@@ -6,3 +6,12 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 import AppTypography from "../ui/AppTypography";
 import axiosInstance from "../../api/axiosInstance";
+
+
+const PostModal = ({ open, post, onClose }) => {
+  const [isLiked, setIsLiked] = useState(false);
+  const [likesCount, setLikesCount] = useState(0);
+  const BE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+
+  +
+  
