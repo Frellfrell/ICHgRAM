674a160d78388a73d8463b16421cceaf65d7726d
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-31 17:50:08 +0200

    Update LikeButton.jsx

--- ICHgRAM_front/src/components/ui/LikeButton.jsx
+++ ICHgRAM_front/src/components/ui/LikeButton.jsx
@@ -4,3 +4,12 @@ import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import AppTypography from "./AppTypography";
 import axiosInstance from "../../api/axiosInstance";
+
+
+const LikeButton = ({ postId, initialLikesCount, showCount = true }) => {
+  const [liked, setLiked] = useState(false);
+  const [count, setCount] = useState(initialLikesCount || 0);
+
+  useEffect(() => {
+    // Проверяем, лайкнул ли текущий юзер этот пост
+    const checkStatus = async () => {
