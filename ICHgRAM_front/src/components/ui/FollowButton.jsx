538d51eea1bb404a338422d4c935c953d16b9d11
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-31 17:38:09 +0200

    Update FollowButton.jsx

--- ICHgRAM_front/src/components/ui/FollowButton.jsx
+++ ICHgRAM_front/src/components/ui/FollowButton.jsx
@@ -1,3 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { Button } from "@mui/material";
 import axiosInstance from "../../api/axiosInstance";
+
+
+
+
+const FollowButton = ({ userId, initialIsFollowing }) => {
+  const [isFollowing, setIsFollowing] = useState(initialIsFollowing);
+  const [loading, setLoading] = useState(false);
