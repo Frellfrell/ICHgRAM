533c3b663ce7d18451f32ee3fe39e1d0a1aa2926
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-31 18:07:22 +0200

    Update PostItem.jsx

--- ICHgRAM_front/src/components/ui/PostItem.jsx
+++ ICHgRAM_front/src/components/ui/PostItem.jsx
@@ -5,3 +5,24 @@ import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
 
 const PostGridItem = ({ post, onClick }) => {
   const BE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+
+  return (
+    <Box 
+      onClick={() => onClick(post)}
+      sx={{
+        position: 'relative',
+        aspectRatio: '1/1',
+        cursor: 'pointer',
+        '&:hover .overlay': { opacity: 1 }
+      }}
+    
+    >
+
+
+
+
+
+
+
+
+    </Box>
