9daf42b5e929ef4cace2e24a841a1aa7b7f05b10
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-31 16:54:41 +0200

    Update Explorer.jsx

---
+++ ICHgRAM_front/src/pages/explorer/Explorer.jsx
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from "react";
+import { Box, CircularProgress } from "@mui/material";
+import MainLayout from "../../layouts/MainLayout";
+import axiosInstance from "../../api/axiosInstance";
+import PostModal from "../../components/posts/PostModal";
