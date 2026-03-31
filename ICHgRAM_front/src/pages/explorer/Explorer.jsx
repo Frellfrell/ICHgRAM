848f76b11b9717092b00dcfedb9f1849e7ca726c
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-31 21:27:36 +0200

    Update Explorer.jsx

--- ICHgRAM_front/src/pages/explorer/Explorer.jsx
+++ ICHgRAM_front/src/pages/explorer/Explorer.jsx
@@ -24,7 +24,7 @@ const Explorer = () => {
 
         setPosts(masonryPosts);
       } catch (err) {
-        console.error("Ошибка при загрузке Explorer:", err);
+        console.error("Ошибка при загрузке Explore:", err);
       } finally {
         setLoading(false);
       }
