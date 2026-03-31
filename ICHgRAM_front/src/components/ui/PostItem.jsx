0364e1d6e997817506acaaab0b683c67b6422b95
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-31 18:12:45 +0200

    Update PostItem.jsx

--- ICHgRAM_front/src/components/ui/PostItem.jsx
+++ ICHgRAM_front/src/components/ui/PostItem.jsx
@@ -1,46 +1,61 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
 
 const PostGridItem = ({ post, onClick }) => {
   const BE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
   return (
-    <Box 
+    <Box
       onClick={() => onClick(post)}
       sx={{
-        position: 'relative',
-        aspectRatio: '1/1',
-        cursor: 'pointer',
-        '&:hover .overlay': { opacity: 1 }
+        position: "relative",
+        aspectRatio: "1/1",
+        cursor: "pointer",
+        "&:hover .overlay": { opacity: 1 },
       }}
-    
     >
-
-        <Box
+      <Box
         component="img"
-        src={post.image.startsWith('data') ? post.image : `${BE_URL}${post.image}`}
-        sx={{ width: '100%', height: '100%', objectFit: 'cover' }}
+        src={
+          post.image.startsWith("data") ? post.image : `${BE_URL}${post.image}`
+        }
+        sx={{ width: "100%", height: "100%", objectFit: "cover" }}
       />
 
       {/* Overlay при наведении */}
-      <Box 
+      <Box
         className="overlay"
         sx={{
-          position: 'absolute', top: 0, left: 0, width: '100%', height: '100%',
-          bgcolor: 'rgba(0,0,0,0.3)', display: 'flex', justifyContent: 'center',
-          alignItems: 'center', opacity: 0, transition: '0.3s', color: 'white', gap: 3
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          bgcolor: "rgba(0,0,0,0.3)",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          opacity: 0,
+          transition: "0.3s",
+          color: "white",
+          gap: 3,
         }}
       >
-
-        
-
-
-
-
-
-
-
-
+        <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+          <FavoriteIcon />{" "}
+          <Typography sx={{ fontWeight: 700 }}>{post.likesCount}</Typography>
+        </Box>
+        <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+          <ChatBubbleIcon />{" "}
+          <Typography sx={{ fontWeight: 700 }}>
+            {post.commentsCount || 0}
+          </Typography>
+        </Box>
+      </Box>
     </Box>
+  );
+};
+
+export default PostGridItem;
