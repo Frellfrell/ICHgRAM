6cbc68c809bd9b09a45868d9f75eae35dd60551a
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-31 16:56:58 +0200

    Update Explorer.jsx

--- ICHgRAM_front/src/pages/explorer/Explorer.jsx
+++ ICHgRAM_front/src/pages/explorer/Explorer.jsx
@@ -3,3 +3,18 @@ import { Box, CircularProgress } from "@mui/material";
 import MainLayout from "../../layouts/MainLayout";
 import axiosInstance from "../../api/axiosInstance";
 import PostModal from "../../components/posts/PostModal";
+
+
+
+const Explorer = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [selectedPost, setSelectedPost] = useState(null);
+
+
+
+  const BE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
