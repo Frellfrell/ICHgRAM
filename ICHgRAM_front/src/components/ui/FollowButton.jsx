f63fe632a6aaf97e659ed4fc5ef3201fc0494671
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-31 17:41:10 +0200

    Update FollowButton.jsx

--- ICHgRAM_front/src/components/ui/FollowButton.jsx
+++ ICHgRAM_front/src/components/ui/FollowButton.jsx
@@ -19,3 +19,7 @@ const FollowButton = ({ userId, initialIsFollowing }) => {
         await axiosInstance.delete(`/api/follows/${userId}`);
         setIsFollowing(false);
       } else {
+        await axiosInstance.post(`/api/follows/${userId}`);
+        setIsFollowing(true);
+      }
+      
