729a3da451ad55dded5e7b30b36b6f03aebe3c13
Frellfrell <ella.gubareva.24@gmail.com>
2026-03-31 17:49:07 +0200

    Update LikeButton.jsx

---
+++ ICHgRAM_front/src/components/ui/LikeButton.jsx
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from "react";
+import { IconButton, Box } from "@mui/material";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import AppTypography from "./AppTypography";
+import axiosInstance from "../../api/axiosInstance";
