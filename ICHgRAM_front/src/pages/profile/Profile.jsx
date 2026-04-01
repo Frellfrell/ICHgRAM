e22b664ffb96372de4390383ffb708c16645229f
Frellfrell <ella.gubareva.24@gmail.com>
2026-04-01 13:22:56 +0200

    Update Profile.jsx

---
+++ ICHgRAM_front/src/pages/profile/Profile.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import {
+  Box,
+  Button,
+  Grid,
+  Divider,
+  CircularProgress,
+  Link,
+} from "@mui/material";
+import MainLayout from "../../layouts/MainLayout";
+import AppTypography from "../ui/AppTypography";
+import AppAvatar from "../ui/AppAvatar";
+import FollowButton from "../ui/FollowButton";
+import PostModal from "../posts/PostModal";
+import axiosInstance from "../../api/axiosInstance";
+import { formatUrl } from "../ui/helpers";
+import LanguageIcon from "@mui/icons-material/Language";
+import SettingsIcon from "@mui/icons-material/Settings";
