94c7c874d63fe23d854b0db689949e8c414c5079
Frellfrell <ella.gubareva.24@gmail.com>
2026-04-01 15:21:48 +0200

    Update EditProfile.jsx

---
+++ ICHgRAM_front/src/pages/profile/EditProfile.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+import { Box, TextField, Button, Avatar, IconButton } from "@mui/material";
+import MainLayout from "../../layouts/MainLayout";
+import AppTypography from "../ui/AppTypography";
+import axiosInstance from "../../api/axiosInstance";
+import { formatUrl } from "../ui/helpers";
+import { useNavigate } from "react-router-dom";
