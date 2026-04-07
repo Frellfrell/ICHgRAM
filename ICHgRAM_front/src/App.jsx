8e9b4c50be9f92fb2e59c82b52adb5739f0f2d98
Frellfrell <ella.gubareva.24@gmail.com>
2026-04-07 17:51:57 +0200

    Update App.jsx

--- ICHgRAM_front/src/App.jsx
+++ ICHgRAM_front/src/App.jsx
@@ -14,7 +14,7 @@ import Login from "./pages/auth/Login.jsx";
 import Register from "./pages/auth/Register.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import AuthLayout from "./layout/AuthLayout.jsx";
-import AuthProvider from "./context/AuthProvider";
+import { AuthProvider } from "./context/AuthProvider";
 import { AuthContext } from "./context/AuthContext";
 import ResetPassword from "./pages/auth/ResetPassword.jsx";
 import Home from "./pages/home/Home.jsx";
