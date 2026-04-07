700694336df00ef1eb6271d344ae5c51beb1683c
Frellfrell <ella.gubareva.24@gmail.com>
2026-04-07 21:42:22 +0200

    Update CreatePostModal.jsx

---
+++ ICHgRAM_front/src/components/create/CreatePostModal.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useRef } from "react";
+import {
+  Modal,
+  Box,
+  Button,
+  Divider,
+  TextField,
+  IconButton,
+  Avatar,
+  Fade,
+  Backdrop,
+} from "@mui/material";
