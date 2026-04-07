963749bdbc3d197cc1c5f83278c6ed79de210ca9
Frellfrell <ella.gubareva.24@gmail.com>
2026-04-07 15:21:04 +0200

    Update AuthContext.jsx

--- ICHgRAM_front/src/context/AuthContext.jsx
+++ ICHgRAM_front/src/context/AuthContext.jsx
@@ -2,3 +2,10 @@ import { createContext, useState, useEffect } from "react";
 import axiosInstance from "../api/axiosInstance.js";
 
 export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isAuth, setIsAuth] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+
+  
