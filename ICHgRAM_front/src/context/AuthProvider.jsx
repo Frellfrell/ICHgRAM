c5919a26b63b1a017ba361bb234252299a2714cd
Frellfrell <ella.gubareva.24@gmail.com>
2026-04-07 15:44:35 +0200

    Update AuthProvider.jsx

--- ICHgRAM_front/src/context/AuthProvider.jsx
+++ ICHgRAM_front/src/context/AuthProvider.jsx
@@ -1,3 +1,22 @@
 import axiosInstance from "../api/axiosInstance.js";
 import { useState, useEffect } from "react";
 import { AuthContext } from "./AuthContext";
+
+
+const AuthProvider = ({ children }) => {
+  const [isAuth, setIsAuth] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setIsAuth(!!token);
+    setLoading(false);
+  }, []);
+
+  const login = () => setIsAuth(true);
+
+const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    setIsAuth(false);
+  };
