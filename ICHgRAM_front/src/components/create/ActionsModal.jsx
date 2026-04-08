33123ee0420a6ce1187557a5b570c2766fc0dd4f
Frellfrell <ella.gubareva.24@gmail.com>
2026-04-08 13:10:55 +0200

    Update ActionsModal.jsx

--- ICHgRAM_front/src/components/create/ActionsModal.jsx
+++ ICHgRAM_front/src/components/create/ActionsModal.jsx
@@ -1,2 +1,19 @@
 import React from "react";
 import { Modal, Box, Button, Divider, Fade, Backdrop } from "@mui/material";
+
+
+const ActionsModal = ({ open, onClose, onDelete, onEdit }) => {
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+      closeAfterTransition
+      slots={{ backdrop: Backdrop }}
+      slotProps={{
+        backdrop: {
+          sx: { backgroundColor: "rgba(0, 0, 0, 0.65)" },
+          timeout: 500,
+        },
+      }}
+      sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+    ></Modal>
