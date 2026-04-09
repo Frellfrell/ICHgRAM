490c1aeaf821c99daf05c24f6ec1cb4f64aa0ee6
Frellfrell <ella.gubareva.24@gmail.com>
2026-04-09 10:59:13 +0200

    Update postApi.js

--- ICHgRAM_front/src/api/postApi.js
+++ ICHgRAM_front/src/api/postApi.js
@@ -1,6 +1,6 @@
 import axiosInstance from "./axiosInstance";
 
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+//const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 /*export const getAllPosts = async () => {
   //const token = localStorage.getItem("token");
@@ -10,9 +10,11 @@ const BASE_URL = import.meta.env.VITE_BASE_URL;
   return response.data; // Получаем массив постов из seed.js
 };*/
 
-export const fetchAllPosts = async () => {
+export const fetchAllPosts = async (page = 1, limit = 4) => {
   try {
-    const response = await axiosInstance.get("/api/posts");
+    const response = await axiosInstance.get(
+      `/api/posts?page=${page}&limit=${limit}`,
+    );
     return response.data;
   } catch (error) {
     console.error("Error fetching posts:", error);
