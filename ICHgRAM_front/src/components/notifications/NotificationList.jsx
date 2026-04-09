5c92551c39db3f9befca61c4ee5ca3ab633acbac
Frellfrell <ella.gubareva.24@gmail.com>
2026-04-09 20:55:47 +0200

    Update NotificationList.jsx

--- ICHgRAM_front/src/components/notifications/NotificationList.jsx
+++ ICHgRAM_front/src/components/notifications/NotificationList.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
 import AppAvatar from "../ui/AppAvatar";
+import { formatUrl } from "../ui/helpers";
+import { useNavigate } from "react-router-dom";
 
 const NotificationList = ({ notifications }) => {
   const BE_URL = "http://localhost:5000";
+  const navigate = useNavigate();
 
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
@@ -19,14 +22,12 @@ const NotificationList = ({ notifications }) => {
             borderBottom: "1px solid rgba(219,219,219,1)",
           }}
         >
-          <AppAvatar
-            src={
-              note.sender?.avatar?.startsWith("data")
-                ? note.sender.avatar
-                : `${BE_URL}${note.sender?.avatar}`
-            }
-            size={44}
-          />
+          <Box
+            onClick={() => navigate(`/profile/${note.sender._id}`)}
+            sx={{ cursor: "pointer" }}
+          >
+            <AppAvatar src={formatUrl(note.sender?.avatar)} size={44} />
+          </Box>
 
           <Box sx={{ flex: 1 }}>
             <Typography sx={{ fontSize: "14px" }}>
@@ -37,22 +38,23 @@ const NotificationList = ({ notifications }) => {
             </Typography>
           </Box>
 
-          {note.post && (
-            <Box
-              component="img"
-              src={
-                note.post.image?.startsWith("data")
-                  ? note.post.image
-                  : `${BE_URL}${note.post.image}`
-              }
-              sx={{
-                width: 44,
-                height: 44,
-                objectFit: "cover",
-                borderRadius: "4px",
-              }}
-            />
-          )}
+          <Box
+            onClick={() => navigate(`/post/${note.post._id}`)}
+            sx={{ cursor: "pointer" }}
+          >
+            {note.post && (
+              <Box
+                component="img"
+                src={formatUrl(note.post.image)}
+                sx={{
+                  width: 44,
+                  height: 44,
+                  objectFit: "cover",
+                  borderRadius: "4px",
+                }}
+              />
+            )}
+          </Box>
         </Box>
       ))}
     </Box>
