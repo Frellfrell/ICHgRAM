080aaca36b233a7a7a59c16a13dea001dcdfebdc
Frellfrell <ella.gubareva.24@gmail.com>
2026-04-11 21:54:57 +0200

    Update ChatRoom.jsx

---
+++ ICHgRAM_front/src/components/message/ChatRoom.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect, useContext, useRef } from "react";
+import {
+  Box,
+  Typography,
+  Avatar,
+  TextField,
+  IconButton,
+  Paper,
+  Divider,
+} from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
