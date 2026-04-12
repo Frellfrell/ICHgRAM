88feb77a38d356a43b2dd461bee752f92f057da3
Frellfrell <ella.gubareva.24@gmail.com>
2026-04-12 12:42:32 +0200

    Update SocketContext.jsx

--- ICHgRAM_front/src/context/SocketContext.jsx
+++ ICHgRAM_front/src/context/SocketContext.jsx
@@ -1,10 +1,22 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import { AuthContext } from "./AuthContext";
-import { formatUrl } from "../components/ui/helpers";
+import { BE_URL } from "../components/ui/helpers";
 
 export const SocketContext = createContext(null);
 
 export const SocketProvider = ({ children }) => {
   const { isAuth } = useContext(AuthContext);
   const [socket, setSocket] = useState(null);
+
+
+  useEffect(() => {
+    if (isAuth) {
+      const token = localStorage.getItem("token");
+
+      const newSocket = io(BE_URL, {
+        auth: { token },
+      });
+
+        setSocket(newSocket);
+        
