ac27083b5ccd8676a740ba5078b95e8e0da04d05
Frellfrell <ella.gubareva.24@gmail.com>
2026-04-12 19:19:53 +0200

    Update ChatSidebar.jsx

--- ICHgRAM_front/src/components/message/ChatSidebar.jsx
+++ ICHgRAM_front/src/components/message/ChatSidebar.jsx
@@ -11,7 +11,7 @@ import {
   CircularProgress,
 } from "@mui/material";
 import axiosInstance from "../../api/axiosInstance";
-import { formatUrl } from "../../utils/formatUrl";
+import { formatUrl } from "../../helpers.js";
 
 const ChatSidebar = ({ onSelectChat, selectedChatId }) => {
   const [contacts, setContacts] = useState([]);
