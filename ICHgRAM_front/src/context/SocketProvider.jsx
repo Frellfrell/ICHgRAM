affc22f6034b34a64fa84ab1f3ef096306d7f6ef
Frellfrell <ella.gubareva.24@gmail.com>
2026-04-12 20:13:54 +0200

    Update SocketProvider.jsx

--- ICHgRAM_front/src/context/SocketProvider.jsx
+++ ICHgRAM_front/src/context/SocketProvider.jsx
@@ -1,46 +1,31 @@
-import React, { createContext, useContext, useEffect, useRef } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import { AuthContext } from "./AuthContext.jsx";
+import { SocketContext } from "./SocketContext";
 import { BE_URL } from "../components/ui/helpers.js";
-
-export const SocketContext = createContext(null);
 
 const SocketProvider = ({ children }) => {
   const { isAuth } = useContext(AuthContext);
-  const socketRef = useRef(null);
+  const [socket, setSocket] = useState(null);
 
   useEffect(() => {
     if (!isAuth) return;
     const token = localStorage.getItem("token");
 
-    socketRef.current = io(BE_URL, {
+    const newSocket = io(BE_URL, {
       auth: { token },
     });
 
+    setSocket(newSocket);
+
     return () => {
-      socketRef.current?.disconnect();
-      socketRef.current = null;
-    };
-  }, [isAuth]);
-
-  {
-    /*const newSocket = io(BE_URL, {
-        auth: { token },
-      });
-
-      setSocket(newSocket);
-
-       return () => {
       newSocket.disconnect();
       setSocket(null);
     };
-  }, [isAuth]);*/
-  }
+  }, [isAuth]);
 
   return (
-    <SocketContext.Provider value={socketRef.current}>
-      {children}
-    </SocketContext.Provider>
+    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
   );
 };
 
